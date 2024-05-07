c964a1142ca8aa42ba59c340c5139e40b6734283
Adanson2022 <adanson2010@hotmail.com>
2024-05-07 18:15:59 -0300

    Quarto commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,17 @@ export function App() {
     <div>
       <Post
         author="Bruno Mito"
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam atque velit impedit! Libero ratione rem ipsum minus consequuntur aliquam, inventore ullam dolorum iste quod deleniti aut! Fugiat assumenda aliquid voluptates."
+        content="Brunão do Angelim, comedor de KitKat."
       />
 
       <Post 
         author="Marcos Hackeado"
         content="Engenheiro de Computação que conseguiu ter seu instagram hackeado."
+      />
+
+      <Post
+        author="Thiago Moedas"
+        content="Homem que Dorme."
       />
     </div>
 
