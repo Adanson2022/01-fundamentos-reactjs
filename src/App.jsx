6d1a5d03f9dd8892fc449394f169071922a2b045
Adanson2022 <adanson2010@hotmail.com>
2024-05-15 17:22:45 -0300

    oitavo commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,25 @@ import styles from './App.module.css';
 import './global.css';
 
 export function App() {
-
   return (
     <div>
       <Header />
 
-      <div className={styles.wrapper}></div>
-       <aside>
-        SideBar
-       </aside>
-       <main>
-        <Post
-          author="Ádanson Fernando"
-          content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem officiis mollitia maxime, atque dolorum vero odio ut magnam beatae in minima quasi incidunt facere optio, minus similique laudantium eligendi nobis."
-        />
-        <Post
-          author="Bruno Lima"
-          content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem officiis mollitia maxime, atque dolorum vero odio ut magnam beatae in minima quasi incidunt facere optio, minus similique laudantium eligendi nobis."
-        />
-       </main>
+      <div className={ styles.wrapper }>
+        <aside>
+          SideBar
+        </aside>
+        <main>
+          <Post
+            author="Ádanson Fernando"
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem officiis mollitia maxime, atque dolorum vero odio ut magnam beatae in minima quasi incidunt facere optio, minus similique laudantium eligendi nobis."
+          />
+          <Post
+            author="Bruno Lima"
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem officiis mollitia maxime, atque dolorum vero odio ut magnam beatae in minima quasi incidunt facere optio, minus similique laudantium eligendi nobis."
+          />
+        </main>
+      </div>
     </div>
 
   )
