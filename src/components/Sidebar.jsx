7bd6a403a945a5ba9a923706daf7216a1bb5784f
Adanson2022 <adanson2010@hotmail.com>
2024-05-17 18:06:19 -0300

    décimo commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,23 @@ import styles from './Sidebar.module.css';
 
 export function Sidebar () {
     return (
-        <aside className={styles.sidebar}></aside>
+        <aside className={styles.sidebar}>
+            <img 
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1563089145-599997674d42?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="" 
+            />
+        
+            <div className={styles.profile}>
+                <strong>Ádanson Fernando</strong>
+                <span>Engenheiro de Computação</span>
+            </div>
+
+            <footer>
+                <a href='#'>
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
     );
 }
