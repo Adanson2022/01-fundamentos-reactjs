0e9ee423292e4a9e40dbc66f5c2f933f464652f6
Adanson2022 <adanson2010@hotmail.com>
2024-06-24 18:19:45 -0300

    Commit: Estrutura da aplicação - Estilizações do Post Parte 1

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,8 +5,25 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}></div>
-                <img className={styles.avatar} src="http://m.gettywallpapers.com/wp-content/uploads/2023/05/Pfp-of-Naruto.jpg" />
-            </header>       
+                    <img className={styles.avatar} src="http://m.gettywallpapers.com/wp-content/uploads/2023/05/Pfp-of-Naruto.jpg" />
+                    <div className={styles.authorInfo}>
+                        <strong> Ádanson Fernando </strong>
+                        <spam> Engenheiro de Computação </spam>
+                    </div>
+
+                    <time title="24 de junho às 16:36h" dateTime="24-06-2024 16:36"> Publicado a 1h </time>
+            </header>
+
+            <div className={styles.content}>
+                <p> Fala galeraa 👋</p>
+                <p> Acabei de subir mais um projeto no meu portifa. É um projeto que eu fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare.</p>
+                <p> 👉{' '}<a href="">jane.design/doctorcare</a> </p>
+                <p> 
+                    <a href="">#novoprojeto</a>{' '}
+                    <a href="">#nlw</a>{' '}
+                    <a href="">#rocketseat</a>
+                </p>
+            </div>       
         </article>  
     )
 }
